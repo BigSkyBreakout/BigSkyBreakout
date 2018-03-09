ca29faa400e1e6e471717bf28192ec413f24aa4d
Stupid-Steven <34665292+Stupid-Steven@users.noreply.github.com>
2018-03-08 19:27:28 -0700

    Add files via upload

---
+++ Legend of Fire Code and Notes/KeyCabinet.ino
@@ -0,0 +1,29 @@
+// Pin for the 3 keys
+int keyCabinet0 = 8;
+
+// Pin for Relay
+int keyCabinetRelay = 9;
+
+
+void setup() {
+  pinMode(keyCabinet0, OUTPUT);
+  pinMode(keyCabinetRelay, OUTPUT);
+  // STOP RELAY (to turn it on, use digitalWrite([name of relay], HIGH);)
+  digitalWrite(keyCabinetRelay, LOW);
+  Serial.begin(9600);
+}
+
+void loop() {
+  int valueSet0 = digitalRead(keyCabinet0);
+  if(valueSet0 == HIGH){
+    digitalWrite(keyCabinetRelay, HIGH);
+    
+  }
+  else{
+    digitalWrite(keyCabinetRelay, LOW);
+  }
+  
+
+}
+
+
