f62c568f3b82badf6c1ae9fc65843d9087e53db3
Stupid-Steven <34665292+Stupid-Steven@users.noreply.github.com>
2018-03-08 20:36:04 -0700

    Update KeyCabinet.ino

--- Legend of Fire Code and Notes/KeyCabinet.ino
+++ Legend of Fire Code and Notes/KeyCabinet.ino
@@ -1,10 +1,10 @@
 // Pin for the 3 keys
-int keyCabinet0 = 8;
+int keyCabinet0 = 8; // Pin
 
 // Pin for Relay
-int keyCabinetRelay = 9;
+int keyCabinetRelay = 9; // Pin
 
-
+// defaults the relay to off with digitalWrite and sets pins 8 and 9 to be outputs 
 void setup() {
   pinMode(keyCabinet0, OUTPUT);
   pinMode(keyCabinetRelay, OUTPUT);
@@ -12,7 +12,7 @@ void setup() {
   digitalWrite(keyCabinetRelay, LOW);
   Serial.begin(9600);
 }
-
+// When switch is pressed, activate the relay
 void loop() {
   int valueSet0 = digitalRead(keyCabinet0);
   if(valueSet0 == HIGH){
@@ -20,6 +20,7 @@ void loop() {
     
   }
   else{
+    // Resets it when the switch isn't pressed
     digitalWrite(keyCabinetRelay, LOW);
   }
   
